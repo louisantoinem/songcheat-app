abc3fc7aa853e972bd05c6dc79daf057986aefa6
Louis Antoine <louisantoinem@gmail.com>
2017-12-28 08:35:40 +0100

    Inline rhythms not displayed in Rhythm component

--- src/Rhythm.jsx
+++ src/Rhythm.jsx
@@ -14,7 +14,7 @@ let Renderer = window.Vex.Flow.Renderer
 
 class Rhythm extends Component {
 
-  constructor (props) {
+  constructor(props) {
     super(props)
     this.state = {
       errors: [],
@@ -22,48 +22,48 @@ class Rhythm extends Component {
     }
   }
 
-  vextab () {
+  vextab() {
     let errors = []
     let warnings = []
 
     for (let rhythm of this.props.rhythms) {
-      try {
-        // parse and render rhythm score with vextab
-        console.info('Converting rhythm to vextab score...')
-        let score = 'options tempo=' + this.props.songcheat.signature.tempo + ' player=false tab-stems=false tab-stem-direction=up\n'
-        score += SongcheatVexTab.Notes2Stave(this.props.songcheat, 0, rhythm.compiledScore, true, 'top', 'Rhythm ' + (
-        rhythm.name || rhythm.id), 1, true, false) + ' options space=20'
+      if (!rhythm.inline) {
+        try {
+          // parse and render rhythm score with vextab
+          console.info('Converting rhythm to vextab score...')
+          let score = 'options tempo=' + this.props.songcheat.signature.tempo + ' player=false tab-stems=false tab-stem-direction=up\n'
+          score += SongcheatVexTab.Notes2Stave(this.props.songcheat, 0, rhythm.compiledScore, true, 'top', 'Rhythm ' + rhythm.name, 1, true, false) + ' options space=20'
 
-        // register warning if not a whole number of bars
-        if (rhythm.duration % this.props.songcheat.barDuration) {
-          let warning = 'Rhythm ' + (
-          rhythm.name || rhythm.id) + ' is currently equivalent to ' + Math.floor(rhythm.duration / this.props.songcheat.barDuration) + ' bar(s) and ' + Utils.durationcodes(rhythm.duration % this.props.songcheat.barDuration) + '. A rhythm unit should be equivalent to a whole number of bars.'
-          warnings.push(warning)
+          // register warning if not a whole number of bars
+          if (rhythm.duration % this.props.songcheat.barDuration) {
+            let warning = 'Rhythm ' + rhythm.name + ' is currently equivalent to ' + Math.floor(rhythm.duration / this.props.songcheat.barDuration) + ' bar(s) and ' + Utils.durationcodes(rhythm.duration % this.props.songcheat.barDuration) + '. A rhythm unit should be equivalent to a whole number of bars.'
+            warnings.push(warning)
+          }
+
+          console.info('Parsing score...')
+          let artist = new Artist(10, 10, 600, {scale: 1.0})
+          let vextab = new VexTab(artist)
+          vextab.parse(score)
+
+          console.info('Rendering score...')
+          artist.render(new Renderer(document.getElementById('canvas.' + rhythm.id), Renderer.Backends.CANVAS))
+
+          console.info('Score done!')
+        } catch (e) {
+          console.error(e)
+          errors.push(e.message)
         }
-
-        console.info('Parsing score...')
-        let artist = new Artist(10, 10, 600, {scale: 1.0})
-        let vextab = new VexTab(artist)
-        vextab.parse(score)
-
-        console.info('Rendering score...')
-        artist.render(new Renderer(document.getElementById('canvas.' + rhythm.id), Renderer.Backends.CANVAS))
-
-        console.info('Score done!')
-      } catch (e) {
-        console.error(e)
-        errors.push(e.message)
       }
     }
 
     this.setState({errors: errors, warnings: warnings})
   }
 
-  componentDidMount () {
+  componentDidMount() {
     this.vextab()
   }
 
-  componentDidUpdate (prevProps, prevState) {
+  componentDidUpdate(prevProps, prevState) {
     if (prevProps.songcheat !== this.props.songcheat || !Utils.arraysEqual(prevProps.rhythms, this.props.rhythms)) {
       this.vextab()
     } else {
@@ -71,11 +71,11 @@ class Rhythm extends Component {
     }
   }
 
-  render () {
+  render() {
     return (<div>
       {this.state.errors.map((error, index) => <p className='error' key={index}>{error}</p>)}
       {this.state.warnings.map((warning, index) => <p className='warning' key={index}>{warning}</p>)}
-      {this.props.rhythms.map(rhythm => <canvas key={rhythm.id} id={'canvas.' + rhythm.id} />)}
+      {this.props.rhythms.map(rhythm => rhythm.inline ? '' : <canvas key={rhythm.id} id={'canvas.' + rhythm.id}/>)}
     </div>)
   }
 }
