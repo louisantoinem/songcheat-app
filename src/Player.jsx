f9a6158f6d8c6f258f2d5e8b57e2c3f9064d5526
Louis Antoine <louisantoinem@gmail.com>
2018-01-11 19:05:52 +0100

    Added options panel in Player, visible only when playing or paused, with tempo and volume sliders.
    TODO: shuffle - if applicable, mode, wave form

--- src/Player.jsx
+++ src/Player.jsx
@@ -40,7 +40,9 @@ class PlayerUI extends Component {
 
     if (this.props.rhythm) this.player.setMode(this.player.MODE_RHYTHM)
 
-    this.state = { countdown: '' }
+    this.state = {
+      countdown: ''
+    }
   }
 
   play () {
@@ -60,15 +62,49 @@ class PlayerUI extends Component {
 
   rewind () {
     this.player.rewind()
+    this.forceUpdate()
+  }
+
+  tempo (speedpct) {
+    this.player.speed(parseInt(speedpct, 10))
+    this.forceUpdate()
+  }
+
+  volume (volume) {
+    this.player.setVolume(parseInt(volume, 10))
+    this.forceUpdate()
   }
 
   render () {
     return <div className='Player'>
-      {this.player.stopped || this.player.paused ? <a onClick={() => this.play()}>&#9658;</a> : null}
-      {this.player.stopped || this.player.paused ? null : <a onClick={() => this.pause()}>&#10074;&#10074;</a>}
-      {this.player.stopped ? null : <a onClick={() => this.stop()}>&#9724;</a>}
-      {this.player.stopped ? null : <a onClick={() => this.rewind()}>&#9668;</a>}
+
       <span className='countdown'>{this.state.countdown}</span>
+
+      {
+        this.player.stopped ? null : <div className='options'>
+
+          <div>
+            <label>Tempo: </label>
+            <input type='range' min='1' max='200' value={this.player.speedpct} onInput={(e) => this.tempo(e.target.value)} onChange={(e) => this.tempo(e.target.value)} />
+            <span>{this.player.getTempo()} bpm</span>
+            <a onClick={() => this.tempo(100)}>Original</a>
+          </div>
+
+          <div>
+            <label>Volume: </label>
+            <input type='range' min='0' max='100' value={this.player.volume} onInput={(e) => this.volume(e.target.value)} onChange={(e) => this.volume(e.target.value)} />
+          </div>
+
+        </div>
+      }
+
+      <div className='controls'>
+        {this.player.stopped || this.player.paused ? <a onClick={() => this.play()}>&#9658;</a> : null}
+        {this.player.stopped || this.player.paused ? null : <a onClick={() => this.pause()}>&#10074;&#10074;</a>}
+        {this.player.stopped ? null : <a onClick={() => this.stop()}>&#9724;</a>}
+        {this.player.stopped ? null : <a onClick={() => this.rewind()}>&#9668;</a>}
+      </div>
+
     </div>
   }
 }
