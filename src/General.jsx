fc7038c073e3d0db17da9545ca1c1b96c4b6acb0
Louis Antoine <louisantoinem@gmail.com>
2018-09-13 20:46:52 +0200

    General: * Video, tutorial and source are displayed as an embedded video if applicable, otherwise as a simple link (opening a new tab). Dependency on react-player. * Displays: time signature, shuffle, key, difficulty.

--- src/General.jsx
+++ src/General.jsx
@@ -1,4 +1,5 @@
 import React, {Component} from 'react'
+import ReactPlayer from 'react-player'
 
 import './General.css'
 
@@ -10,9 +11,30 @@ class General extends Component {
       <h1>{this.props.songcheat.title}</h1>
       <h3>{this.props.songcheat.artist}{this.props.songcheat.year ? ', ' + this.props.songcheat.year : ''}</h3>
       <p>{this.props.songcheat.comment}</p>
-      <h3>Capo: {this.props.songcheat.capo > 0 ? this.props.songcheat.capo : 'n/a'}</h3>
-      <h3>Tuning: {this.props.songcheat.tuning}</h3>
-      {/* TODO: Show: difficulty, video, tutorial, key, time, shuffle (with svg image) */}
+
+      {this.props.songcheat.video && <div>
+        {ReactPlayer.canPlay(this.props.songcheat.video) && <p><ReactPlayer controls url={this.props.songcheat.video} /></p>}
+        {!ReactPlayer.canPlay(this.props.songcheat.video) && <a target='blank_' href={this.props.songcheat.video} >{this.props.songcheat.video}</a>}
+      </div>}
+
+      <p><b>Signature</b> {this.props.songcheat.signature.time.symbol} {!this.props.songcheat.signature.shuffle ? '' : (this.props.songcheat.signature.shuffle === ':4' ? '/ Shuffled quarter notes' : '/ Shuffled ' + this.props.songcheat.signature.shuffle.substr(1) + 'th notes')}
+        {this.props.songcheat.signature.shuffle === ':8' && <img width='60' style={{display: 'inline', marginLeft: '10px'}} alt='shuffled 8th' src='/shuffle_8th.svg' />}
+      </p>
+
+      <p><b>Key</b> {this.props.songcheat.signature.key} / <b>Capo</b> {this.props.songcheat.capo > 0 ? this.props.songcheat.capo : '0'} / <b>Tuning</b> {this.props.songcheat.tuning}</p>
+
+      <p><b>Difficulty</b> {this.props.songcheat.difficulty}/5</p>
+
+      {this.props.songcheat.tutorial && <div>
+          {ReactPlayer.canPlay(this.props.songcheat.tutorial) && <p><ReactPlayer controls url={this.props.songcheat.tutorial} /></p>}
+          {!ReactPlayer.canPlay(this.props.songcheat.tutorial) && <p>Tutorial: <a target='blank_' href={this.props.songcheat.tutorial} >{this.props.songcheat.tutorial}</a></p>}
+        </div>}
+
+      {this.props.songcheat.source && <div>
+        {ReactPlayer.canPlay(this.props.songcheat.source) && <p><ReactPlayer controls url={this.props.songcheat.source} /></p>}
+        {!ReactPlayer.canPlay(this.props.songcheat.source) && <p>Source: <a target='blank_' href={this.props.songcheat.source} >{this.props.songcheat.source}</a></p>}
+      </div>}
+
     </div>
   }
 }
