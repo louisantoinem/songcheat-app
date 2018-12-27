87afe527fd142376c5d0fc739389d94e3f69f49d
Louis Antoine <louisantoinem@gmail.com>
2018-12-28 00:46:20 +0100

    Browser: favorites * new collection "ratings" { user_id, songcheat_id, favorite }, can be enriched later with new properties related to a (songcheat x user) * when logged in, one can mark a songcheat as favorite by clicking on the star (method toggleFavorite) * added a filter "Favorite only", orthogonal to the "mine" filter and full-text search * removed blue border and text for recently modified songcheats (only for recently created songcheats), replaced by bold text in info span * fixed a bug: when logged out, do not consider "mine" and "favorite" filters stored in local storage

--- src/Browser.jsx
+++ src/Browser.jsx
@@ -27,12 +27,14 @@ export default class Browser extends Component {
     super(props)
     this.stitchClient = this.props.stitchClient
     this.songcheats = this.props.songcheats
+    this.ratings = this.props.ratings
     this.mutex = new Mutex()
     this.loaded = null
 
     let defaultSettings = {
       'Search.search': '',
-      'Search.mine': false
+      'Search.mine': false,
+      'Search.favorite': false
     }
 
     // load stored settings if any
@@ -43,6 +45,7 @@ export default class Browser extends Component {
     for (let k in defaultSettings) if (typeof settings[k] === 'undefined') settings[k] = defaultSettings[k]
 
     this.state = {
+      favorites: Map(),
       data: null,
       settings: Map(settings)
     }
@@ -51,8 +54,9 @@ export default class Browser extends Component {
   async load () {
     if (this.stitchClient.isAuthenticated()) {
       let search = this.state.settings.get('Search.search')
-      let mine = this.state.settings.get('Search.mine')
-      let what = `${mine ? 'my documents' : 'all documents'} matching "${search}"`
+      let mine = this.props.authed() ? this.state.settings.get('Search.mine') : false
+      let favorite = this.props.authed() ? this.state.settings.get('Search.favorite') : false
+      let what = `${mine ? 'my' : 'all'} ${favorite ? 'favorite documents' : 'documents'} matching "${search}"`
       if (this.loaded === this.state.settings) console.warn(`Already loaded ${what}`)
       else {
         this.setState({ data: null })
@@ -64,8 +68,14 @@ export default class Browser extends Component {
         let or = [ { artist: { $regex: regex } }, { title: { $regex: regex } }, { type: { $regex: regex } }, { source: { $regex: regex } } ]
         let sort = { type: 1, artist: 1, year: 1}
         let data = await this.songcheats.find(mine ? { owner_id: this.stitchClient.authedId(), $or: or } : { $or: or }).sort(sort).execute()
+
+        // get favorite songcheats for this user by songcheat_id
+        let ratings = await this.ratings.find({ user_id: this.stitchClient.authedId() }).execute()
+        let favorites = new window.Map()
+        for (let rating of ratings) if (rating.favorite) favorites.set(rating.songcheat_id.toString(), true)
+        favorites = Map(favorites)
         console.warn(`Done listing ${what}`)
-        this.setState({ data: this.groupByType(data) })
+        this.setState({ favorites, data: this.groupByType(data, favorite ? favorites : null) })
       }
     }
   }
@@ -78,19 +88,22 @@ export default class Browser extends Component {
     if (prevState.settings !== this.state.settings) this.mutex.runExclusive(() => this.load())
   }
 
-  groupByType (data) {
-    let lastType = null
-    let groupedData = { length: data.length, dataByType: new window.Map() }
+  groupByType (data, keep) {
+    // initialize result
+    let groupedData = {
+      length: data.length,
+      dataByType: new window.Map()
+    }
 
+    // group by type, keeping only given item ids if any and listing distinct artists on the way
     for (let item of data) {
-      let type = item.type || '(unknown type)'
-      if (type !== lastType) {
-        groupedData.dataByType.set(type, { artists: new window.Map(), items: [] })
-        lastType = type
+      if (keep && !keep.get(item._id.toString())) groupedData.length--
+      else {
+        let type = item.type || '(unknown type)'
+        if (!groupedData.dataByType.get(type)) groupedData.dataByType.set(type, { artists: new window.Map(), items: [] })
+        groupedData.dataByType.get(type).items.push(item)
+        groupedData.dataByType.get(type).artists.set(item.artist, 1)
       }
-
-      groupedData.dataByType.get(type).items.push(item)
-      groupedData.dataByType.get(type).artists.set(item.artist, 1)
     }
 
     // sort types by descending number of items
@@ -105,6 +118,21 @@ export default class Browser extends Component {
     return groupedData
   }
 
+  async toggleFavorite (songcheat_id) {
+    let favorite = !this.state.favorites.get(songcheat_id.toString())
+    console.warn('Toggle songcheat ' + songcheat_id + ' to favorite = ' + favorite)
+    this.setState({ favorites: this.state.favorites.set(songcheat_id.toString(), favorite) })
+    if (!this.props.authed()) throw new Error('Cannot save favorite: not logged in')
+
+    let document = {
+      user_id: this.stitchClient.authedId(),
+      songcheat_id: songcheat_id,
+      favorite: favorite
+    }
+
+    return await this.ratings.updateOne({ 'user_id': this.stitchClient.authedId(), 'songcheat_id': songcheat_id }, { '$set': document }, { upsert: true })
+  }
+
   itemTemplate (item) {
     if (!item) return
     let created_days = Math.round(Math.abs(((new Date()).getTime() - item.created.getTime()) / (24 * 60 * 60 * 1000)))
@@ -113,20 +141,21 @@ export default class Browser extends Component {
       <div title={'Created ' + timeago.ago(item.created) + ' / Modified ' + timeago.ago(item.last_modified)} className={'item' + (created_days <= 30 ? ' created' : '') + (last_modified_days <= 30 ? ' last_modified' : '')} key={item._id}>
         <Link to={'/' + item._id}>
           <span className='artist'>{item.artist + (item.year ? ' (' + item.year + ')' : '')}</span>
-          <span className='title'>{item.title}</span>
+          <span className='title'>{item.title} </span>
           <span className='info'><i className='fa fa-edit' /> {timeago.ago(item.last_modified)}</span>
         </Link>
+        {this.props.authed() && <i className={'fa fa-star ' + (this.state.favorites.get(item._id.toString()) ? 'favorite' : '')} onClick={() => this.toggleFavorite(item._id)} />}
       </div>
     )
   }
 
   items (data) {
     let items = []
-    for (let row of data) {
-      items.push(<div className='items' key={row[0]}>
-        <h3>{row[0]}</h3>
-        <h4>{row[1].items.length} {row[1].items.length > 1 ? 'titles' : 'title'} / {row[1].artists.size} {row[1].artists.size > 1 ? 'artists' : 'artist'}</h4>
-        { row[1].items.map(item => { return this.itemTemplate(item) }) }
+    for (let entry of data) {
+      items.push(<div className='items' key={entry[0]}>
+        <h3>{entry[0]}</h3>
+        <h4>{entry[1].items.length} {entry[1].items.length > 1 ? 'titles' : 'title'} / {entry[1].artists.size} {entry[1].artists.size > 1 ? 'artists' : 'artist'}</h4>
+        { entry[1].items.map(item => { return this.itemTemplate(item) }) }
       </div>)
     }
     return items
@@ -152,6 +181,8 @@ export default class Browser extends Component {
           <InputText onChange={(e) => this.updateSetting('Search.search', e.target.value)} value={this.state.settings.get('Search.search')} placeholder='Search...' style={{width: '30%'}} />
           { this.props.authed() && <Checkbox onChange={(e) => this.updateSetting('Search.mine', e.checked)} checked={this.state.settings.get('Search.mine')} style={{marginLeft: '.25em'}} /> }
           { this.props.authed() && <label>Mine only</label> }
+          { this.props.authed() && <Checkbox onChange={(e) => this.updateSetting('Search.favorite', e.checked)} checked={this.state.settings.get('Search.favorite')} style={{marginLeft: '.5em'}} /> }
+          { this.props.authed() && <label>Favorites only</label> }
         </div>
         <div className='p-toolbar-group-right' />
       </Toolbar>
@@ -159,7 +190,7 @@ export default class Browser extends Component {
       { !this.state.data && <div className='SpinnerContainer'><ProgressSpinner style={{width: '150px', height: '150px'}} strokeWidth='8' fill='#EEEEEE' animationDuration='1s' /></div> }
       { this.state.data &&
         <div>
-          <h2>{this.state.data.length} songcheats found {this.loaded.get('Search.search') ? 'matching "' + this.loaded.get('Search.search') + '"' : ''}</h2>
+          <h2>{this.state.data.length} {this.loaded.get('Search.favorite') ? ' favorite ' : ''} songcheats found {this.loaded.get('Search.search') ? 'matching "' + this.loaded.get('Search.search') + '"' : ''}</h2>
           { this.items(this.state.data.dataByType) }
         </div>
       }
--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { Route } from 'react-router';
-import { BrowserRouter } from 'react-router-dom';
+import { Route } from 'react-router'
+import { BrowserRouter } from 'react-router-dom'
 import registerServiceWorker from './registerServiceWorker'
-import { StitchClientFactory } from "mongodb-stitch";
+import { StitchClientFactory } from 'mongodb-stitch'
 
 // app components
 import Auth from './Auth'
@@ -11,57 +11,57 @@ import App from './App.jsx'
 import Browser from './Browser.jsx'
 import './index.css'
 
-let appId = "songcheat-stitch-irqmn";
-let mongodbService = "mongodb-atlas";
-let options = {};
+let appId = 'songcheat-stitch-irqmn'
+let mongodbService = 'mongodb-atlas'
+let options = {}
 
-if (process.env.APP_ID) appId = process.env.APP_ID;
-if (process.env.MONGODB_SERVICE) mongodbService = process.env.MONGODB_SERVICE;
+if (process.env.APP_ID) appId = process.env.APP_ID
+if (process.env.MONGODB_SERVICE) mongodbService = process.env.MONGODB_SERVICE
 if (process.env.STITCH_URL) options.baseUrl = process.env.STITCH_URL;
 
 (async function () {
-
   // create audio context
   let audioCtx = new (window.AudioContext || window.webkitAudioContext || window.audioContext)()
 
   // get stitch client connection
   let stitchClient = await StitchClientFactory.create(appId, options)
-  
+
   // auhenticate as guest if not authed yet
   if (!stitchClient.isAuthenticated()) await stitchClient.authenticate('anon')
 
   // get user profile data
   let userData = await stitchClient.userProfile()
-  
+
   // authed() returns true if authed, not as a guest
-  let authed = () => { 
+  let authed = () => {
     for (let identity of userData.identities) {
       if (identity.provider_type !== 'anon-user') return true
     }
     return false
   }
-  
-  // get handle on songcheats Collection
-  let db = stitchClient.service("mongodb", mongodbService).db("songcheat");
-  let songcheats = db.collection("songcheats");
 
-  let props = { audioCtx, stitchClient, songcheats, authed };
+  // get handle on songcheats Collection
+  let db = stitchClient.service('mongodb', mongodbService).db('songcheat')
+  let songcheats = db.collection('songcheats')
+  let ratings = db.collection('ratings')
+
+  let props = { audioCtx, stitchClient, songcheats, ratings, authed }
 
   ReactDOM.render(
     <BrowserRouter>
       <div>
-        <Route exact path="/" render={routeProps => 
+        <Route exact path='/' render={routeProps =>
           <div>
-            <Auth {...props} {...routeProps}/>
-            <Browser {...props} {...routeProps}/>
+            <Auth {...props} {...routeProps} />
+            <Browser {...props} {...routeProps} />
           </div>
-        }/>
-        <Route path="/:_id" render={routeProps => 
+        } />
+        <Route path='/:_id' render={routeProps =>
           <div>
-            <Auth {...props} {...routeProps}/>
-            <App {...props} {...routeProps}/>
+            <Auth {...props} {...routeProps} />
+            <App {...props} {...routeProps} />
           </div>
-        }/>
+        } />
       </div>
     </BrowserRouter>,
     document.getElementById('root'))
