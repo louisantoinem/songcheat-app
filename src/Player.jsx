369610eb8de0da2c96f4937ba978ca26f26bb507
Louis Antoine <louisantoinem@gmail.com>
2020-04-14 16:27:38 +0200

    Corrigé layout en columns pour FF en commentant column-fill: https://bugzilla.mozilla.org/show_bug.cgi?id=733808 Essayé de corriger le carré noir top petit dans le player en changeant le code unicode, mais marche pas

--- src/Player.jsx
+++ src/Player.jsx
@@ -111,7 +111,7 @@ class PlayerUI extends Component {
       <div className='controls'>{this.player.minutes()}:{(this.player.seconds() + '').padStart(2, '0')}
         {this.player.stopped || this.player.paused ? <a onClick={() => this.play()}>&#9658;</a> : null}
         {this.player.stopped || this.player.paused ? null : <a onClick={() => this.pause()}>&#10074;&#10074;</a>}
-        {this.player.stopped ? null : <a onClick={() => this.stop()}>&#9724;</a>}
+        {this.player.stopped ? null : <a onClick={() => this.stop()}>&#9632;</a>}
         {this.player.stopped ? null : <a onClick={() => this.player.rewind()}>&#9668;</a>}
       </div>}
 
